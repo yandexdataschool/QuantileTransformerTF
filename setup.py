906773c09a8295fa0106605c148af4d9996e62c7
Nikita Kazeev <nikita.kazeev@cern.ch>
2018-11-07 13:25:03 +0000

    Version bump to 1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='quantile_transformer_tf',
-    version='1.0',
+    version='1.1',
     description='An implementation of QuantileTransformer in tensorflow',
     long_description=long_description,
     url='https://github.com/yandexdataschool/QuantileTransformerTF',
